287368df427422accb0da36873c97964d6b863fe
Huda Prasetyo <real.hudaprasetyo@gmail.com>
2025-03-08 02:36:02 +0700

    fix: url vercel

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import { server } from './server'
 import { logger } from './app/utils/logger.util'
-import { nodeMailerWrapper } from './app/services/nodemailer-wrapper.service'
-import { DatabaseService } from './app/services/database.service'
+// import { nodeMailerWrapper } from './app/services/nodemailer-wrapper.service'
+// import { DatabaseService } from './app/services/database.service'
 
 if (!process.env.PORT) throw new Error('PORT must be defined')
 if (!process.env.JWT_SECRET) throw new Error('JWT_SECRET must be defined')
@@ -20,12 +20,12 @@ if (!process.env.CLIENT_VERIFY_URL)
 if (!process.env.CLIENT_RESET_PASSWORD_URL)
   throw new Error('CLIENT_RESET_PASSWORD_URL must be defined')
 
-try {
-  await DatabaseService.connect()
-  await nodeMailerWrapper.connect()
-} catch (err) {
-  logger.error(err)
-}
+// try {
+//   await DatabaseService.connect()
+//   await nodeMailerWrapper.connect()
+// } catch (err) {
+//   logger.error(err)
+// }
 
 server.listen(process.env.PORT || 3000, () => {
   logger.info(`Listening on port ${process.env.PORT}`)
